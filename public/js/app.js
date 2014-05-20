3d1a7223461e8bd1f17fa03020ff9750abe1aec9
Nicolas Lound <nicolas.lound@gmail.com>
2014-05-20 15:21:18 -0300

    Limpio archivos huerfanos

--- public/js/app.js
+++ public/js/app.js
@@ -1,139 +1,249 @@
-var sql = cartodb.SQL({
-	user : 'gcba'
+sql = cartodb.SQL({
+    user: 'gcba'
 });
 
-/*
- * Inicializacion del mapa
- */
+function init(asd) {
+    var sql = sql;
 
-/*
- * En el submit del form agrega un punto
- */
- 
-$("form").submit(function() {
-	// Estilo de INSERT 
-	// http://{account}.cartodb.com/api/v2/sql?q=INSERT INTO test_table (column_name, column_name_2, the_geom) VALUES ('this is a string', 11, ST_SetSRID(ST_Point(-110, 43),4326))&api_key={Your API key}
+    var map = L.map('map', {
+        zoomControl: false,
+        center: [-34.59682955724048, -58.45533370971679],
+        zoom: 13
+    });
+    mapaPrincipal = map;
 
-	var cuenta = "gcba",
-		columnas = "the_geom,telefono,descripcion,servicios,inicio_de_actividades,calle,piso_dpto,tipo,web,responsable_proyecto,mail_responsable,mail_institucional,nombre,tags,cartodb_georef_status,pendiente_revision",
-		tabla = "mapa_emprendedores",
-		apikey = "15ea5821068feecc0584c70d07355848537c2182";
+    L.tileLayer('https://a.tiles.mapbox.com/v3/gcbadata.map-0ub2e509/{z}/{x}/{y}.png', {
+        attribution: "<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>"
+    }).addTo(map);
+
+    var layerUrl = 'http://gcba.cartodb.com/api/v2/viz/196d1632-d0ad-11e3-bbd2-0e230854a1cb/viz.json';
+
+    var sublayers = [];
+
+    $("button").click(function (d) {
+        switch (d.currentTarget.id) {
+        case "list-view":
+            busquedaListado();
+            break;
+        }
+    });
+
+    function listarMaps(query) {
+
+        var contenido = $('#modal-list .modal-body');
+
+        var q = "SELECT * FROM mapa_emprendedores";
+        sql.execute(q).done(function (data) {
+            for (var i = 0; i < data.total_rows; i++) {
+                contenido.append("<div> <span>" + data.rows[i].nombre + " (" + data.rows[i].tipo + ")");
+                contenido.children('.loading').remove();
+            }
+        }).error(function (errors) {
+            console.log("SQL ERR:", errors);
+        });
+    }
+
+    function listarTipos() {
+
+        var contenido = $('#modal-list .modal-body');
+
+        var q = "SELECT * FROM mapa_emprendedores";
+        sql.execute(q).done(function (data) {
+            for (var i = 0; i < data.total_rows; i++) {
+                contenido.append("<div> <span>" + data.rows[i].nombre + " (" + data.rows[i].tipo + ")");
+                contenido.children('.loading').remove();
+            }
+        }).error(function (errors) {
+            console.log("SQL ERR:", errors);
+        });
+
+    }
 
 
-		var tempLatLong = $("#latLong_frm").val();
-		tempLatLong = tempLatLong.replace(",", " ");
-		valores=  "'" +
-			"SRID=4326;POINT(" + tempLatLong + ")','" +
-			$("#tele_frm").val() + "','" + 
-			$("#desc_frm").val() + "','" +
-			$("#serv_frm").val() + "','" +
-			$("#acti_frm").val() + "','" +
-			$("#direccion_frm").val() + "','" +
-			$("#piso_frm").val() + "','" +
-			$("#tipo_frm").val() + "','" +
-			$("#web_frm").val() + "','" +
-			$("#resp_frm").val() + "','" +
-			$("#mailRes_frm").val() + "','" +
-			$("#mailIns_frm").val() + "','" +
-			$("#nombre_frm").val() + "','" +
-			$("#tags_frm").val() + "','true',false" ;
-	
-	var url = "http://" + cuenta + ".cartodb.com/api/v2/sql?q=INSERT INTO " + tabla + " (" + columnas + ") VALUES (" + valores + ")&api_key=" + apikey; 
-	$.post(url);
+    var filterPath = function (path) {
+        if (path == "null") {
+            return "all";
+        } else if (path == "Universidad") {
+            return "universidades";
+        } else if (path == "Universidad") {
+            return "universidades";
+        } else if (path == "Makerspace") {
+            return "makerspace";
+        } else if (path == "Incubadora") {
+            return "incubadora"
+        } else if (path == "Espacio") {
+            return "coworking";
+        } else if (path == "Fondo") {
+            return "fondo_inversion"
+        } else if (path == "Emprendimiento") {
+            return "emprendimiento"
+        } else if (path == "Aceleradora") {
+            return "aceleradora"
+        } else if (path == "Organización") {
+            return "organización"
+        }
+    }
 
-	$('#modal-form').modal('hide');		
-				
-	return false; // para que Chrome no recargue la página
-});
+    function generateTypeList() {
+        var queryList = "SELECT distinct tipo FROM mapa_emprendedores";
+        //SELECT * FROM mapa_emprendedores WHERE tipo IN ('Espacio de Coworking','Emprendimiento','Aceleradora','Fondo de Inversión','Incubadora','Makerspace','Universidad')
+        var contenido = $('#lista-emprendimientos');
+        var tipoForm = $('#tipo_frm');
 
-$("button").click(function(d) {
-	switch (d.currentTarget.id) {
-		case "list-view":
-			busquedaListado();
-			break;
-	}
-});
+        sql.execute(queryList).done(function (data) {
+            contenido.append("<li><a href='#'> Ver Todos </a></li>");
+            for (var i = 0; i < data.total_rows; i++) {
+                //
+                tipoForm.append("<option>" + data.rows[i].tipo + "</option>");
+                //console.log(data.rows[i].tipo[0])
+                var id_path = filterPath(data.rows[i].tipo.split(' ')[0]);
+                //class="button megacities selected
+                if (id_path == "universidad") {
+                    console.log(id_path)
+                    contenido.append("<li><a href=#" + id_path + " id=" + id_path + " class='button " + id_path + "' selected>" + data.rows[i].tipo + "</a></li>");
+                } else {
+                    contenido.append("<li><a href=#" + id_path + " id=" + id_path + " class='button " + id_path + "'>" + data.rows[i].tipo + "</a></li>");
+                }
+            }
 
+        }).error(function (errors) {
+            console.log("SQL ERR:", errors);
+        });
+    }
 
-/*
- * Query SQL para ver los detalles de la empresa seleccionada.
- */
-function verDetallesEmpresa (par) {
-	$('#modal-list').modal('show');		
-	var contenido = $('#modal-list .modal-body');
-	var idEmpresa = par.replace('emp', ''); 
-	var q = "SELECT * FROM mapa_emprendedores WHERE cartodb_id = " + idEmpresa ;
-	contenido.children('div').remove();
-	sql.execute(q).done(function(data) {
-		for (var i = 0; i < data.total_rows; i++) {
-			contenido.append(
-				"<div class='list-group'>"+
-					"<span class='list-group-item'>" +
-						"<h4 class='list-group-item-heading'>" + data.rows[i].nombre +
-						" <small> (" + data.rows[i].tipo + ")</small></h4>" + 
-					    "<p class='list-group-item-text'>" + data.rows[i].descripcion + "</p>" +
-					"</span>" +
-				"</div>"
-			);
+    function generateYears() {
+        var currentTime = new Date();
+        var desde = 1990,
+            hasta = currentTime.getFullYear();
 
-			console.log ();
+        var contenido = $('#acti_frm');
+        for (var i = desde; i <= hasta; i++) {
+            contenido.append("<option>" + i + "</option>");
+        }
+    }
 
-			
-			
-			$.get("http://gcba.cartodb.com/api/v1/sql?q=select st_y(the_geom) as lat, st_x(the_geom) as lon from mapa_emprendedores WHERE cartodb_id = " + idEmpresa , function(data) {
-				for(var i = 0; i < data.rows.length; ++i) {
-					var row = data.rows[i];
-				    console.log("point", row.lat, row.lon);
-					mapaPrincipal.panTo(L.latLng(row.lat, row.lon));
-					mapaPrincipal.setZoom(15);
-				}
-			});			
-			
-			  
-			
+    generateYears();
 
-			contenido.children('.loading').remove();
-		}
-	}).error(function(errors) {
-		console.log("SQL ERR:", errors);
-	});
+    /*
+     * updatea la busqueda por keyword
+     */
+    $("#busquedaEmprendedores").keyup(function () {
+        busquedaKeyword($('#busquedaEmprendedores').val());
+    });
+
+    var LayerActions = {
+        all: function () {
+            sublayers[0].setSQL("SELECT * FROM mapa_emprendedores ");
+            return true;
+        },
+        universidades: function () {
+            sublayers[0].setSQL("SELECT * FROM mapa_emprendedores WHERE tipo IN ('Universidad')");
+            return true;
+        },
+        makerspace: function () {
+            sublayers[0].setSQL("SELECT * FROM mapa_emprendedores WHERE tipo IN ('Makerspace')");
+            return true;
+        },
+        incubadora: function () {
+            sublayers[0].setSQL("SELECT * FROM mapa_emprendedores WHERE tipo IN ('Incubadora')");
+            return true;
+        },
+        coworking: function () {
+            sublayers[0].setSQL("SELECT * FROM mapa_emprendedores WHERE tipo IN ('Espacio de Coworking')");
+            return true;
+        },
+        fondo_inversion: function () {
+            sublayers[0].setSQL("SELECT * FROM mapa_emprendedores WHERE tipo IN ('Fondo de Inversión')");
+            return true;
+        }
+    }
+
+    cartodb.createLayer(map, layerUrl)
+        .addTo(map)
+        .on('done', function (layer) {
+            // change the query for the first layer
+            var subLayerOptions = {
+                sql: "SELECT * FROM mapa_emprendedores",
+            }
+
+            var sublayer = layer.getSubLayer(0);
+
+            sublayer.set(subLayerOptions);
+
+            sublayers.push(sublayer);
+        }).on('error', function () {
+            //log the error
+        });
+
+    $('.button').click(function () {
+        $('.button').removeClass('selected');
+        $(this).addClass('selected');
+        LayerActions[$(this).attr('id')]();
+    });
 }
 
+window.onload = init
 
-
-/*
- * Query SQL para el listado total.
- */
 function busquedaListado() {
 
-	var contenido = $('#modal-list .modal-body');
+    var contenido = $('#modal-list .modal-body');
 
-	var q = "SELECT * FROM mapa_emprendedores WHERE pendiente_revision = true";
-	contenido.children('div').remove();
-	
-	sql.execute(q).done(function(data) {
-		for (var i = 0; i < data.total_rows; i++) {
+    var q = "SELECT * FROM mapa_emprendedores WHERE pendiente_revision = true";
+    contenido.children('div').remove();
 
-			contenido.append(
-				"<div id='emp" + data.rows[i].cartodb_id + "' onclick='verDetallesEmpresa(this.id)'><a href='#' class='list-group-item'> "+ 
-					"<span>" +
-						data.rows[i].nombre +
-						" <small>(" + data.rows[i].tipo + ")</small>" +
-					"<span></a></div>"
-			);
-
-			contenido.children('.loading').remove();
-
-		}
-	}).error(function(errors) {
-		console.log("SQL ERR:", errors);
-	});
+    sql.execute(q).done(
+        function (data) {
+            for (var i = 0; i < data.total_rows; i++) {
+                contenido.append(
+                    "<div id='emp" + data.rows[i].cartodb_id + "' onclick='verDetallesEmpresa(this.id)'><a href='#' class='list-group-item'> " +
+                    "<span>" +
+                    data.rows[i].nombre +
+                    " <small>(" + data.rows[i].tipo + ")</small>" +
+                    "<span></a></div>"
+                );
+                contenido.children('.loading').remove();
+            }
+        }).error(
+        function (errors) {
+            console.log("SQL ERR:", errors);
+        });
 }
 
-/*
- * Hace un query a la base de emprendedores con lo que se
- * escriba en el input CASE SENSITIVE
- */
+function verDetallesEmpresa(par) {
+    $('#modal-list').modal('show');
+    var contenido = $('#modal-list .modal-body');
+    var idEmpresa = par.replace('emp', '');
+    var q = "SELECT * FROM mapa_emprendedores WHERE cartodb_id = " + idEmpresa;
+    contenido.children('div').remove();
+    sql.execute(q).done(function (data) {
+        for (var i = 0; i < data.total_rows; i++) {
+            contenido.append(
+                "<div class='list-group'>" +
+                "<span class='list-group-item'>" +
+                "<h4 class='list-group-item-heading'>" + data.rows[i].nombre +
+                " <small> (" + data.rows[i].tipo + ")</small></h4>" +
+                "<p class='list-group-item-text'>" + data.rows[i].descripcion + "</p>" +
+                "</span>" +
+                "</div>"
+            );
+
+            $.get("http://gcba.cartodb.com/api/v1/sql?q=select st_y(the_geom) as lat, st_x(the_geom) as lon from mapa_emprendedores WHERE cartodb_id = " + idEmpresa, function (data) {
+                for (var i = 0; i < data.rows.length; ++i) {
+                    var row = data.rows[i];
+                    console.log("point", row.lat, row.lon);
+                    mapaPrincipal.panTo(L.latLng(row.lat, row.lon));
+                    mapaPrincipal.setZoom(15);
+                }
+            });
+
+            contenido.children('.loading').remove();
+        }
+    }).error(function (errors) {
+        console.log("SQL ERR:", errors);
+    });
+}
+
+
 function busquedaKeyword(key) {
 	var contenido = $('#busquedaList');
 
@@ -163,50 +273,3 @@ function busquedaKeyword(key) {
 		});
 	}
 }
-
-/*
- * Llena el selector de FILTRAR EMPRENDIMIENTOS mediante un query
- * a la base de datos por el campo tipo
- */
-function generateTypeList() {
-	var queryList = "SELECT distinct tipo FROM mapa_emprendedores WHERE pendiente_revision = true";
-
-	var contenido = $('#lista-emprendimientos');
-	var tipoForm = $('#tipo_frm');
-
-	sql.execute(queryList).done(function(data) {
-	contenido.append("<li><a href='#'> Ver Todos </a></li>");
-		for (var i = 0; i < data.total_rows; i++) {
-
-			tipoForm.append("<option>" + data.rows[i].tipo + "</option>");
-
-			contenido.append("<li><a href='#'>" + data.rows[i].tipo + "</a></li>");
-		}
-
-	}).error(function(errors) {
-		console.log("SQL ERR:", errors);
-	});
-}
-
-function generateYears() {
-	var currentTime = new Date();
-	var desde = 1990, hasta = currentTime.getFullYear();
-
-	var contenido = $('#acti_frm');
-	for (var i = desde; i <= hasta; i++) {
-		contenido.append("<option>" + i + "</option>");
-	}
-}
-
-generateYears();
-generateTypeList();
-
-/*
- * updatea la busqueda por keyword
- */
-
-//si vacio entonces nada
-$("#busquedaEmprendedores").keyup(function() {
-	busquedaKeyword($('#busquedaEmprendedores').val());
-});
-
