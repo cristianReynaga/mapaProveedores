87236f64c072f4b9c46f8aa1dbb22ee641cb3725
Nicolás Lound <nicolas.lound@gmail.com>
2014-06-18 12:23:39 -0300

    Create cartodb.config.php

---
+++ libs/cartodb.config.php
@@ -0,0 +1,14 @@
+<?php
+
+function getConfig() {
+  $config = array();
+
+  $config['key'] = 'key';
+  $config['secret'] = 'secret';
+  $config['email'] = 'email';
+  $config['password'] = 'passworsd';
+  $config['subdomain'] = 'subdomail';
+
+  return $config;
+  
+}
