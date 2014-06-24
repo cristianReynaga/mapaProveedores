b53913b0edfeafb3c225878ba66f850377271106
Nicolas Lound <nicolas.lound@gmail.com>
2014-06-24 17:08:51 -0300

    Agrego funcionabilidades de filtros via sql

--- js/init.js
+++ js/init.js
@@ -1,12 +1,9 @@
 'use strict'
 var capas;
-
 // Instacio el mapa
 var visualizacion = cartodb.createVis(mapa, 'http://gcba.cartodb.com/api/v2/viz/623b9526-f7ec-11e3-95ab-0e230854a1cb/viz.json')
-    .done(function(vis, layers) {
-        capas = layers[1];
-
-
+    .done(function(vis) {
+        capas = vis;
     });
 
 
